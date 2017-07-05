37affa05a0d7b408e75ca676c91019c181aba860
Jackson Utsch <jutechs@gmail.com>
2017-07-04 17:15:08 -0700

    Initial Commit

---
+++ cabinet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cabinet
+//
+//  Created by Jackson Utsch on 7/4/17.
+//  Copyright © 2017 Jackson Utsch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ cabinet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cabinet
+//
+//  Created by Jackson Utsch on 7/4/17.
+//  Copyright © 2017 Jackson Utsch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
